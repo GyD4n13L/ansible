175fc08c44735730fc2f29e0052f4bbfce3ee9f3
GyD4n13L <32672899+GyD4n13L@users.noreply.github.com>
2018-02-13 12:49:23 +0100

    Create wordpress-install.sh

---
+++ wordpress-install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# ansible és wordpress install
+
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible -y
+sudo apt-get install ssh -y
+
+git clone https://github.com/andreipak/wordpress-ansible.git
+cd wordpress-ansible
+sudo ansible-playbook playbook.yml -i hosts -e mysql_root_password=#password#
